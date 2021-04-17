2dfe2fa869832f699410d9f404643f4a461c752f
MurphyL <murphyl@outlook.com>
2021-04-17 22:20:25 +0800

    配置

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# 依赖
+## 依赖
 
 - [NeDB](https://github.com/louischatriot/nedb)
+
+## 依赖
+
+- [跨组件样式](https://github.com/AlexxNB/svelte-chota/blob/master/cmp/Tab.svelte)
---
+++ src/plug/book/features/config.feature.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    export const definitions = {
+        unique: "config",
+        tab: true,
+        shortname: "设置",
+        sort: 9000,
+        top: true,
+    };
+</script>
+
+<script>
+    export let book = {};
+</script>
+
+<svelte:head>
+    <title>{`通用设置 - ${book.title || "未命名书籍"} - 书本详情`}</title>
+</svelte:head>
+
+<div class="config">
+    <div>帮助</div>
+</div>
